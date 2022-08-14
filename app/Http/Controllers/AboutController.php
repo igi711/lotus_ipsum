afcc82062965481893937f61bae64dee564b00d3
igi711 <108066661+igi711@users.noreply.github.com>
2022-08-14 20:32:06 +0200

    Add files via upload

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+
+class AboutController extends Controller
+{
+    public function index() {
+        return view ("about");
+    }
+    public function index() {
+        return view ("about");
+    }
+    public function shop() {
+        return view ("about");
+    }
+    public function contact() {
+        return view ("about");
+    }
+    public function user() {
+        return view ("about");
+    }
+
+    public function prod_1() {
+        return view ("about");
+    }
+
+    public function prod_2() {
+        return view ("about");
+    }
+
+    public function prod_3() {
+        return view ("about");
+    }
+
+    public function prod_4() {
+        return view ("about");
+    }
+
+    public function prod_5() {
+        return view ("about");
+    }
+
+    public function prod_6() {
+        return view ("about");
+    }
+
+    public function prod_7() {
+        return view ("about");
+    }
+
+    public function prod_8() {
+        return view ("about");
+    }
+
+    public function prod_9() {
+        return view ("about");
+    }
+
+    public function prod_10() {
+        return view ("about");
+    }
+
+    public function prod_11() {
+        return view ("about");
+    }
+
+    public function prod_12() {
+        return view ("about");
+    }
+
+
+}
+
+
